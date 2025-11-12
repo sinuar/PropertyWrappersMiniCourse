d9920ba2811e35a59195b4a7114af6aceccab9f6
sinuar <sinuar2@gmail.com>
2025-11-12 15:36:58 -0600

    Add mini app with @StateObject, @ObservedObject and @EnvironmentObject

---
+++ MiniApp3Views/MiniApp3Views/MiniApp3ViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniApp3ViewsApp.swift
+//  MiniApp3Views
+//
+//  Created by Sinuhe Alvarez Ruedas on 12/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MiniApp3ViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+        }
+    }
+}
